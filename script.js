b8082c52fd362376985e1c92a05b3b9a4263e178
yogesh <yogeshkondapalli3@gmail.com>
2023-09-09 13:25:35 +0530

    commited

--- script.js
+++ script.js
@@ -14,19 +14,7 @@ function beforesubmit(event) {
   }
 }
 
-function timestamp() {
-  var response = document.getElementById("g-recaptcha-response");
-  if (response == null || response.value.trim() == "") {
-    var elems = JSON.parse(
-      document.getElementsByName("captcha_settings")[0].value
-    );
-    elems["ts"] = JSON.stringify(new Date().getTime());
-    document.getElementsByName("captcha_settings")[0].value =
-      JSON.stringify(elems);
-  }
-}
-setInterval(timestamp, 500);
-
+function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500); 
 function captchasuccess() {
   captchachecked = true;
 }
